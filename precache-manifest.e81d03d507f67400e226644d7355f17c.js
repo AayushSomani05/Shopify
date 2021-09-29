e0c018ff00618c3849d5272654969daee9c26c4b
AayushSomani05 <aayushsomnai002@gmail.com>
2021-09-29 22:13:47 +0530

    Updates

---
+++ precache-manifest.e81d03d507f67400e226644d7355f17c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ac47a76087df6c945fcf78a417e193",
+    "url": "/Shopify/index.html"
+  },
+  {
+    "revision": "a12c85f36af37771a788",
+    "url": "/Shopify/static/css/main.59db51c7.chunk.css"
+  },
+  {
+    "revision": "ba203057c61fcecdc8bc",
+    "url": "/Shopify/static/js/2.0b2e803f.chunk.js"
+  },
+  {
+    "revision": "13e061b2fd50c1d7f7b7e0bb533040d6",
+    "url": "/Shopify/static/js/2.0b2e803f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12c85f36af37771a788",
+    "url": "/Shopify/static/js/main.743184a0.chunk.js"
+  },
+  {
+    "revision": "52b7a7fcd6b8cd59f7cc",
+    "url": "/Shopify/static/js/runtime-main.d1d143d3.js"
+  }
+]);
